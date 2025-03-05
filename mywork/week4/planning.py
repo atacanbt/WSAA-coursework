7579827aa66f7982c2d8f1adbefda2f728810dbe
atacanbt <157822752+atacanbt@users.noreply.github.com>
2025-03-05 17:27:15 +0000

    lec2 completed

--- mywork/week4/planning.py
+++ mywork/week4/planning.py
@@ -1,9 +1,8 @@
 # get planning applications
 
 import requests
-# url = 'https://opendata.arcgis.com/datasets/8f69dffe26324ba3acc653cf6cb5cf8b_0.geojson'
-file_path = 'file:///Users/atacanbuyuktalas/Desktop/Data Analytics/WSAA-coursework/mywork/week4/data/Planning_Application_Points.geojson'
-response = requests.get(file_path)
+url = 'https://opendata.arcgis.com/datasets/8f69dffe26324ba3acc653cf6cb5cf8b_0.geojson'
+response = requests.get(url)
 list_of_planning = response.json()
 print(response.status_code)
 
