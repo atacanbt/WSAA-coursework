b4f37d27a6ff533dbc8d7b3d7cf609122711a752
atacanbt <157822752+atacanbt@users.noreply.github.com>
2025-03-06 14:07:20 +0000

    assignment 3 is done

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,18 @@
+# The program reads Exchequer Account (Historical Series) data from data.cso.ie and store it into cso.json file
+# 
+# Author: Atacan Buyuktalas
+
+import requests
+import json
+
+url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
+
+def get_cso_data(url):
+    response = requests.get(url)
+    data = response.json() 
+
+    with open("cso.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+    print("Dataset successfully saved to cso.json")
+
+get_cso_data(url)
